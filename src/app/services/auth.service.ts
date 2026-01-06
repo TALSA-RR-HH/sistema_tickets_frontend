8ec8176402d6e91022fcf1ab85d68dc46babc9b2
Valentin Fernandez <valenferxux@gmail.com>
2026-01-06 07:53:52 -0500

    Feat: Implementación de AuthService con lógica de login y signals

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { UsuarioDTO } from '../models';
+import { Router } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    // Inyectar dependencias
+    private http = inject(HttpClient);
+    private router = inject(Router);
+
+    private apiUrl = 'http://localhost:8080/api/usuarios';
+
+    currentUser = signal<UsuarioDTO | null>(this.getUserFromStorage());
+
+    // Metodos
+    login(credentials: {username: string, password: string}): Observable<UsuarioDTO> {
+        return this.http.post<UsuarioDTO>(`${this.apiUrl}/login`, credentials).pipe(
+            tap(user => {
+                this.currentUser.set(user);
+                localStorage.setItem('currentUser', JSON.stringify(user));
+            })
+        )
+    }
+
+    logout() {
+        this.currentUser.set(null);
+        localStorage.removeItem('currentUser');
+
+        this.router.navigate(['/login']);
+    }
+
+    private getUserFromStorage(): UsuarioDTO | null {
+        const userStr = localStorage.getItem('currentUser');
+        return userStr ? JSON.parse(userStr) : null;
+    }
+
+    isAuthenticated(): boolean {
+        return this.currentUser() !== null;
+    }
+}
