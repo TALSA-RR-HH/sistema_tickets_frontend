586a6bb9ac4aef4af69821682c44247110df5300
Valentin Fernandez <valenferxux@gmail.com>
2026-01-06 08:12:26 -0500

    Refactor: Separación de login en HTML, TS y CSS

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router } from '@angular/router';
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+    selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+    private authService = inject(AuthService);
+    private router = inject(Router);
+
+    // Variables para el formulario
+    username = '';
+    password = '';
+    errorMessage = '';
+    isLoading = false;
+
+    onLogin() {
+        this.isLoading = true;
+        this.errorMessage = '';
+
+        this.authService.login({username: this.username, password: this.password})
+            .subscribe({
+                next: () => {
+                    this.isLoading = false;
+                    this.router.navigate(['/dashboard']);
+                },
+                error: (err) => {
+                    this.isLoading = false;
+                    this.errorMessage = 'Credenciales incorrectas o usuario inactivo.';
+                    console.error('Login error:', err);
+                }
+            });
+    }
+}
